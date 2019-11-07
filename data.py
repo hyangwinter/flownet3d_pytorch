90ccd60b33ea22df0e465df4c07c88aeb3a23fb9
hao03.wang <hao03.wang@gitlab.hobot.cc>
2019-11-07 16:25:16 +0800

    initial version

---
+++ data.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+import sys
+import glob
+import h5py
+import numpy as np
+from scipy.spatial.transform import Rotation
+from torch.utils.data import Dataset
+
+
+# Part of the code is referred from: https://github.com/charlesq34/pointnet
+
+def download():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
+        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+        zipfile = os.path.basename(www)
+        os.system('wget %s; unzip %s' % (www, zipfile))
+        os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
+        os.system('rm %s' % (zipfile))
+
+
+def load_data(partition):
+    # download()
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, '../../datasets')
+    all_data = []
+    all_label = []
+    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5' % partition)):
+        f = h5py.File(h5_name)
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+
+
+def translate_pointcloud(pointcloud):
+    xyz1 = np.random.uniform(low=2. / 3., high=3. / 2., size=[3])
+    xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
+
+    translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
+    return translated_pointcloud
+
+
+def jitter_pointcloud(pointcloud, sigma=0.01, clip=0.05):
+    N, C = pointcloud.shape
+    pointcloud += np.clip(sigma * np.random.randn(N, C), -1 * clip, clip)
+    return pointcloud
+
+
+class ModelNet40(Dataset):
+    def __init__(self, num_points, partition='train', gaussian_noise=False, unseen=False, factor=4):
+        self.data, self.label = load_data(partition)
+        self.num_points = num_points
+        self.partition = partition
+        self.gaussian_noise = gaussian_noise
+        self.unseen = unseen
+        self.label = self.label.squeeze()
+        self.factor = factor
+        if self.unseen:
+            ######## simulate testing on first 20 categories while training on last 20 categories
+            if self.partition == 'test':
+                self.data = self.data[self.label>=20]
+                self.label = self.label[self.label>=20]
+            elif self.partition == 'train':
+                self.data = self.data[self.label<20]
+                self.label = self.label[self.label<20]
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item][:self.num_points]
+        if self.gaussian_noise:
+            pointcloud = jitter_pointcloud(pointcloud)
+        if self.partition != 'train':
+            np.random.seed(item)
+        anglex = np.random.uniform() * np.pi / self.factor
+        angley = np.random.uniform() * np.pi / self.factor
+        anglez = np.random.uniform() * np.pi / self.factor
+
+        cosx = np.cos(anglex)
+        cosy = np.cos(angley)
+        cosz = np.cos(anglez)
+        sinx = np.sin(anglex)
+        siny = np.sin(angley)
+        sinz = np.sin(anglez)
+        Rx = np.array([[1, 0, 0],
+                        [0, cosx, -sinx],
+                        [0, sinx, cosx]])
+        Ry = np.array([[cosy, 0, siny],
+                        [0, 1, 0],
+                        [-siny, 0, cosy]])
+        Rz = np.array([[cosz, -sinz, 0],
+                        [sinz, cosz, 0],
+                        [0, 0, 1]])
+        # 生成旋转矩阵
+        R_ab = Rx.dot(Ry).dot(Rz)
+        R_ba = R_ab.T
+        # 生成平移向量
+        translation_ab = np.array([np.random.uniform(-0.5, 0.5), np.random.uniform(-0.5, 0.5),
+                                   np.random.uniform(-0.5, 0.5)])
+        translation_ba = -R_ba.dot(translation_ab)
+
+        pointcloud1 = pointcloud.T
+        # 变换点云，为什么这里不直接用R_ab，而是要从欧拉角重新生成旋转矩阵？？？？
+        rotation_ab = Rotation.from_euler('zyx', [anglez, angley, anglex])
+        pointcloud2 = rotation_ab.apply(pointcloud1.T).T + np.expand_dims(translation_ab, axis=1)
+
+        euler_ab = np.asarray([anglez, angley, anglex])
+        euler_ba = -euler_ab[::-1]
+
+        pointcloud1 = np.random.permutation(pointcloud1.T).T
+        pointcloud2 = np.random.permutation(pointcloud2.T).T
+
+        return pointcloud1.astype('float32'), pointcloud2.astype('float32'), R_ab.astype('float32'), \
+               translation_ab.astype('float32'), R_ba.astype('float32'), translation_ba.astype('float32'), \
+               euler_ab.astype('float32'), euler_ba.astype('float32')
+
+    def __len__(self):
+        return self.data.shape[0]
+
+class SceneflowDataset(Dataset):
+    def __init__(self, npoints=2048, root='data_preprocessing/data_processed_maxcut_35_both_mask_20k_2k', partition='train'):
+        self.npoints = npoints
+        self.partition = partition
+        self.root = root
+        if self.partition=='train':
+            self.datapath = glob.glob(os.path.join(self.root, 'TRAIN*.npz'))
+        else:
+            self.datapath = glob.glob(os.path.join(self.root, 'TEST*.npz'))
+        self.cache = {}
+        self.cache_size = 30000
+
+        ###### deal with one bad datapoint with nan value
+        self.datapath = [d for d in self.datapath if 'TRAIN_C_0140_left_0006-0' not in d]
+        ######
+        print(self.partition, ': ',len(self.datapath))
+
+    def __getitem__(self, index):
+        if index in self.cache:
+            pos1, pos2, color1, color2, flow, mask1 = self.cache[index]
+        else:
+            fn = self.datapath[index]
+            with open(fn, 'rb') as fp:
+                data = np.load(fp)
+                pos1 = data['points1'].astype('float32')
+                pos2 = data['points2'].astype('float32')
+                color1 = data['color1'].astype('float32') / 255
+                color2 = data['color2'].astype('float32') / 255
+                flow = data['flow'].astype('float32')
+                mask1 = data['valid_mask1']
+
+            if len(self.cache) < self.cache_size:
+                self.cache[index] = (pos1, pos2, color1, color2, flow, mask1)
+
+        if self.partition == 'train':
+            n1 = pos1.shape[0]
+            sample_idx1 = np.random.choice(n1, self.npoints, replace=False)
+            n2 = pos2.shape[0]
+            sample_idx2 = np.random.choice(n2, self.npoints, replace=False)
+
+            pos1 = pos1[sample_idx1, :]
+            pos2 = pos2[sample_idx2, :]
+            color1 = color1[sample_idx1, :]
+            color2 = color2[sample_idx2, :]
+            flow = flow[sample_idx1, :]
+            mask1 = mask1[sample_idx1]
+        else:
+            pos1 = pos1[:self.npoints, :]
+            pos2 = pos2[:self.npoints, :]
+            color1 = color1[:self.npoints, :]
+            color2 = color2[:self.npoints, :]
+            flow = flow[:self.npoints, :]
+            mask1 = mask1[:self.npoints]
+
+
+        return pos1, pos2, color1, color2, flow, mask1
+
+    def __len__(self):
+        return len(self.datapath)
+
+
+
+
+if __name__ == '__main__':
+    train = ModelNet40(1024)
+    test = ModelNet40(1024, 'test')
+    for data in train:
+        print(data[0].shape)
+        break
---
+++ model.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+import torch
+import numpy as np
+import torch.nn.functional as F
+from util import PointNetSetAbstraction,PointNetFeaturePropogation,FlowEmbedding,PointNetSetUpConv
+
+
+
+class FlowNet3D(nn.Module):
+    def __init__(self,args):
+        super(FlowNet3D,self).__init__()
+
+        self.sa1 = PointNetSetAbstraction(npoint=1024, radius=0.5, nsample=16, in_channel=3, mlp=[32,32,64], group_all=False)
+        self.sa2 = PointNetSetAbstraction(npoint=256, radius=1.0, nsample=16, in_channel=64, mlp=[64, 64, 128], group_all=False)
+        self.sa3 = PointNetSetAbstraction(npoint=64, radius=2.0, nsample=8, in_channel=128, mlp=[128, 128, 256], group_all=False)
+        self.sa4 = PointNetSetAbstraction(npoint=16, radius=4.0, nsample=8, in_channel=256, mlp=[256,256,512], group_all=False)
+        
+        self.fe_layer = FlowEmbedding(radius=10.0, nsample=64, in_channel = 128, mlp=[128, 128, 128], pooling='max', corr_func='concat')
+        
+        self.su1 = PointNetSetUpConv(nsample=8, radius=2.4, f1_channel = 256, f2_channel = 512, mlp=[], mlp2=[256, 256])
+        self.su2 = PointNetSetUpConv(nsample=8, radius=1.2, f1_channel = 128+3, f2_channel = 256, mlp=[128, 128, 256], mlp2=[256])
+        self.su3 = PointNetSetUpConv(nsample=8, radius=0.6, f1_channel = 64, f2_channel = 256, mlp=[128, 128, 256], mlp2=[256])
+        self.fp = PointNetFeaturePropogation(in_channel = 256, mlp = [256, 256])
+        
+        self.conv1 = nn.Conv1d(256, 128, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.conv2=nn.Conv1d(128, 3, kernel_size=1, bias=True)
+        
+    def forward(self, pc1, pc2, feature1, feature2):
+        l1_pc1, l1_feature1 = self.sa1(pc1, feature1)
+        l2_pc1, l2_feature1 = self.sa2(l1_pc1, l1_feature1)
+        
+        l1_pc2, l1_feature2 = self.sa1(pc2, feature2)
+        l2_pc2, l2_feature2 = self.sa2(l1_pc2, l1_feature2)
+        
+        _, l2_feature1_new = self.fe_layer(l2_pc1, l2_pc2, l2_feature1, l2_feature2)
+
+        l3_pc1, l3_feature1 = self.sa3(l2_pc1, l2_feature1_new)
+        l4_pc1, l4_feature1 = self.sa4(l3_pc1, l3_feature1)
+        
+        l3_fnew1 = self.su1(l3_pc1, l4_pc1, l3_feature1, l4_feature1)
+        l2_fnew1 = self.su2(l2_pc1, l3_pc1, torch.cat([l2_pc2, l2_feature1_new], dim=1), l3_fnew1)
+        l1_fnew1 = self.su3(l1_pc1, l2_pc1, l1_feature1, l2_fnew1)
+        l0_fnew1 = self.fp(pc1, l1_pc1, None, l1_fnew1)
+        
+        x = F.relu(self.bn1(self.conv1(l0_fnew1)))
+        sf = self.conv2(x)
+        return sf
+        
+if __name__ == '__main__':
+    import os
+    import torch
+    os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+    input = torch.randn((8,3,2048))
+    label = torch.randn(8,16)
+    model = FlowNet3D()
+    output = model(input,input)
+    print(output.size())
